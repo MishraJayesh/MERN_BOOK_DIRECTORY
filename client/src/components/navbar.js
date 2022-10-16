fe6022e3c031b256f956d345cb0f630035b6a5e2
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-10-16 18:52:32 +0530

    navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,60 +1,31 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import React from 'react';
 
-export default function AppNavbar() {
-  const [showModal, setShowModal] = useState(false);
+function AppNavbar() {
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/">
-            Google Books Search
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar"/>
-          <Navbar.Collapse id="navbar">
-            <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/">
-                Search For Books
-              </Nav.Link>
-                  <Nav.Link as={Link} to="/saved">
-                    See Your Books
-                  </Nav.Link>
-                  <Nav.Link>Logout</Nav.Link>
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal">
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item>
-                  <Nav.Link eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </>
+      <div className='wrapper'>
+        <nav className='nav'>
+          <a href="##" className='logo'>My BoOKs App</a>
+          <ul>
+            <li>
+              <a href="##" className='about'>About</a>
+            </li>
+            <li>
+              <a href="##" className='features'>Features</a>
+            </li>
+            <li>
+              <a href="##" className='pricing'>Pricing</a>
+            </li>
+            <li>
+              <a href="##" className='faq'>FAQ</a>
+            </li>
+            <li>
+              <a href="##" className='contact'>Contacts</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
   );
 };
+
+export default AppNavbar;
