9f19026a6f9841a4bcef8f8c41eb886c455dd6b0
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-10-17 21:48:19 +0530

    Adding Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import AppNavbar from './components/navbar';
-import './App.css';
-
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
-      <AppNavbar />
+      <Navbar />
     </div>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React from "react";
+import './navbar.css';
+import logo from './img/logo.jpg';
 
-function AppNavbar() {
-
+function Navbar() {
   return (
-      <div className='wrapper'>
-        <nav className='nav'>
-          <a href="##" className='logo'>My BoOKs App</a>
-          <ul>
-            <li>
-              <a href="##" className='about'>About</a>
-            </li>
-            <li>
-              <a href="##" className='features'>Features</a>
-            </li>
-            <li>
-              <a href="##" className='pricing'>Pricing</a>
-            </li>
-            <li>
-              <a href="##" className='faq'>FAQ</a>
-            </li>
-            <li>
-              <a href="##" className='contact'>Contacts</a>
-            </li>
-          </ul>
+    <div className="container">
+      <header className="header">
+        <nav className="nav">
+          <p className="brand"><img src={logo} alt ="logo"/>
+             MY BOOKS APP
+          </p>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <div className="wrapper">
+            <a href="##" alt="" className="nav-link">Home</a>
+            <ul>
+              <li>
+                <a href="##" alt="" className="nav-link">Category</a>
+                <ul>
+                  <li><a href="##">Biographies</a></li>
+                  <li><a href="##">Novels</a></li>
+                  <li><a href="##">Science-Fiction</a></li>
+                  <li><a href="##">Factual</a></li>
+                  <li><a href="##">Encyclopedias &
+                    Dictonaries   </a></li>
+                  <li><a href="##">Mythological</a></li>
+                  <li><a href="##">Poetries</a></li>
+                  <li><a href="##">Stories</a></li>
+                  <li><a href="##">Auto-Biographies</a></li>
+                </ul>
+              </li>
+            </ul>
+            <a href="##" alt="" className="nav-link">Pricing</a>
+            <a href="##" alt="" className="nav-link">Contact Us</a>
+            <a href="##" alt="" className="nav-link">FAQs</a>
+          </div>
         </nav>
-      </div>
+      </header>
+    </div>
   );
-};
+}
 
-export default AppNavbar;
+export default Navbar;
